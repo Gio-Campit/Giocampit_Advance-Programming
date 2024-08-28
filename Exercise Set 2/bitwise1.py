32512ef02de25b9493571890d337685c28054555
Gio Campit <23-02645@g.batstate-u.edu.ph>
2024-08-28 12:58:41 +0800

    Add files via upload

---
+++ Exercise Set 2/bitwise1.py
@@ -0,0 +1,11 @@
+result_a = 12|25
+result_b = 21&27
+result_c = 8^7
+result_d = 13&7
+result_e = 13|7
+
+print ("Output 1:", result_a)
+print ("Output 2:", result_b)
+print ("Output 3:", result_c)
+print ("Output 4:", result_d)
+print ("Output 5:", result_e)
---
+++ Exercise Set 2/string-handling.py
@@ -0,0 +1,4 @@
+color = input ("Enter first color: ")
+color_2 = input ("Enter second color: ")
+color_3 = input ("Enter third color: ")
+print (color,("\b,"),color_2,("\b,"),("and"),color_3,("created a new color!") )
