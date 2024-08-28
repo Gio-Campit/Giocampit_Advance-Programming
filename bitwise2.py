26964444dff9e1b903a43d29c71f2a1691fdc541
Gio Campit <23-02645@g.batstate-u.edu.ph>
2024-08-28 12:49:34 +0800

    Add files via upload

---
+++ bitwise2.py
@@ -0,0 +1,6 @@
+a = 21
+b = int(a)
+
+a = input ("Enter an integer: ")
+result_1 = b&30
+print ("21 AND 30 = ", result_1)
---
+++ typecasting.py
@@ -0,0 +1,10 @@
+a = 100
+b = int (a)
+
+c = 92.7
+d = float (b)
+
+b = input ("Enter an integer: ")
+d = input ("Enter a float: ")
+difference = a - c
+print (f"Difference: {difference:.2f}" ) 
